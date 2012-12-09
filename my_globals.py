d13c468e3b14ed838174f1576dd67c658e8dc2e7
Brian Kovacs <brian.kovacs@me.com>
2012-12-09 06:31:45 -0500

    added class Info with global object game

--- my_globals.py
+++ my_globals.py
@@ -9,22 +9,33 @@ else: # white characters
 	diamond = u"\u2662"
 	club = u"\u2667"
 	
-# a list of all possible cards, done as a tuple so that it cannot be accidently changed at runtime
-allcards = tuple([Card(s, c) for s in (diamond, spade, club, heart) for c in (14, 13, 12, 11, 10, 9)])
+class Info:
+    pass
+
+game = Info()
+
+game.dealer = None
+game.trump  = None
+game.lead   = None
+game.teamA  = 0
+game.teamB  = 0
 
 class Card(object):
 	def __init__(self, suit, num):
 		self.suit = suit
 		self.num = num
-		
+    
 	def __str__(self):
 		if self.num > 10:
 			return list("Jack", "Queen", "King", "Ace")[self.num - 11] + "|" + self.suit
 		else:
 			return str(self.num) + "|" + self.suit
-
+    
 	def __repr__(self):
 		return "Card(%r)" % (self.__dict)
+
+# a list of all possible cards, done as a tuple so that it cannot be accidently changed at runtime
+allcards = tuple([Card(s, c) for s in (diamond, spade, club, heart) for c in (14, 13, 12, 11, 10, 9)])
 
 def offSuit(trump_suit):
 	"""
