abcba08c6780cb74254d6034751a4c5cc9ce8f09
Alex Addy <alex.addy@gmail.com>
2012-12-11 11:17:33 -0500

    Fixed the __str__ and __repr__ funcs of Card.

--- my_globals.py
+++ my_globals.py
@@ -32,12 +32,12 @@ class Card(object):
 
 	def __str__(self):
 		if self.num > 10:
-			return list("Jack", "Queen", "King", "Ace")[self.num - 11] + "|" + self.suit
+			return ["Jack", "Queen", "King", "Ace"][self.num - 11] + "|" + self.suit
 		else:
 			return str(self.num) + "|" + self.suit
 
 	def __repr__(self):
-		return "Card(%r)" % (self.__dict)
+		return '<%s %s>' % (self.__class__.__name__, self.__dict__)
 
 # a list of all possible cards, done as a tuple so that it cannot be accidently changed at runtime
 allcards = tuple([Card(s, c) for s in (diamond, spade, club, heart) for c in (14, 13, 12, 11, 10, 9)])
