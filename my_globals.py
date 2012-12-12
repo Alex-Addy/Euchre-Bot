ad9a71125899bd48fa8c23b87395e55a429fe46c
Alexander Addy <alex.addy@gmail.com>
2012-12-12 10:29:11 -0500

    Added a query_yes_no function.

--- my_globals.py
+++ my_globals.py
@@ -67,3 +67,30 @@ def offSuit(trump_suit):
 		return club
 	else:
 		return spade
+		
+def query_yes_no(question, default="yes"):
+    """
+	Ask a yes/no question via raw_input() and return their answer.
+	source: http://stackoverflow.com/a/3041990
+    """
+    valid = {"yes":True,   "y":True,  "ye":True,
+             "no":False,     "n":False}
+			 
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        print (question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print "Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
